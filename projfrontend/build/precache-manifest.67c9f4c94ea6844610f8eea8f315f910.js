a04116884322523b89e5d04e206688ce5055c3ca
akshay-17 <43988784+akshay-17@users.noreply.github.com>
2020-12-23 22:53:01 +0530

    Add files via upload

---
+++ projfrontend/build/precache-manifest.67c9f4c94ea6844610f8eea8f315f910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b625cc59c2d2a594003a71cb68ac7ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ae2cb9b2cb9a455dbaf",
+    "url": "/static/css/main.451e0ed1.chunk.css"
+  },
+  {
+    "revision": "8d42b92a34055e108536",
+    "url": "/static/js/2.61997b1b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.61997b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae2cb9b2cb9a455dbaf",
+    "url": "/static/js/main.851a1af7.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  }
+]);
